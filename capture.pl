1eed511a6d355f5d7e0c0cabf8ad0a7769ba0ced
jonty-comp <jontysewell@gmail.com>
2012-05-16 13:16:49 +0000

    Written to read from an XML file, and store to a log directory with temporary files and atomic renames.  No logging or timescales yet.

--- capture.pl
+++ capture.pl
@@ -1,29 +1,65 @@
 #!/usr/bin/perl
 
-use strict;
 use warnings;
-
 use LWP::Simple;
+use XML::Simple;
+use File::Temp;
+use Data::Dumper;
 
-my $log = '/mnt/webcam-log/';
-my @cameras = ('http://cam-studio1-dj/image.jpg','http://cam-studio1-guest/image.jpg','http://cam-studio2-dj/image.jpg','http://cam-studio2-guest/image.jpg');
+my $log = '/mnt/webcam-log';
 
+sub read_xml {
+	$xml = new XML::Simple (KeyAttr=>[]);
+	$data = $xml->XMLin("cameras.xml");
+}
+
+#LOG: "$log/$_[0]->{id}/".time().".jpg"
 sub get_image {
-	my $id = $_[0] + 1;
-	my $url = $cameras[$_[0]];
-	my $file = '/mnt/webcam-log/'.sprintf("%03d", $id).'-'.time().'.jpg';
+	my $file = "$log/current/$_[0]->{id}.jpg";
+	my $tmp = File::Temp->new( TEMPLATE => "$_[0]->{id}XXXX",
+				   DIR => "$log/tmp/",
+				   SUFFIX => ".jpg");
 
-	my $result = getstore($url, $file);
-
+	my $result = getstore($_[0]->{url}, $tmp->filename);
 	if($result != 200) {
-		print "Error fetching image: ".$result;
+		print "Error fetching $_[0]->{url}: $result\n";
+	} else {
+		rename $tmp->filename, $file;
+		print "Fetched $_[0]->{url} to $file\n";
 	}
 }
 
 sub main {
-	for my $i (0 .. $#cameras) {
- 	   get_image($i);
+	while (1) {
+		foreach $i (@{$data->{camera}}) {
+			$pid = fork();
+			if ($pid) {
+				push(@childs, $pid);
+			} elsif ( $pid == 0 ) {
+				get_image($i);
+				exit(0);
+			} else {
+				die "couldn't fork, probably wills' fault";
+			}
+		}
+		foreach (@childs) {
+			waitpid($_,0);
+		}
+		sleep 1;
 	}
 }
 
+sub init {
+	if(!-d "$log/current") { mkdir("$log/current") or die "Could not make current image store $log/current: $!\n";	}
+	if(!-d "$log/tmp") { mkdir("$log/tmp") or die "Could not make temporary image store $log/tmp: $!\n"; }
+	for my $i (@{$data->{camera}}) {
+		$dir = "$log/$i->{id}";
+		if(!-d $dir) {
+			mkdir($dir) or die "Could not make folder $dir: $!\n";
+		}
+	}
+}
+
+read_xml;
+init;
 main;
